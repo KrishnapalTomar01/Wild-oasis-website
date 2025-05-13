aab59aa6fd634e2be3c10b92b1519a5b8cf799cf
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2025-05-13 21:36:41 +0530

    Styling and organize

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis",
@@ -9,13 +10,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
         </header>
 
         <main>{children}</main>
+
+        <footer>Copyright by The Wild Oasis</footer>
       </body>
     </html>
   );
