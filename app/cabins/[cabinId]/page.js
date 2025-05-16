e24fb5de9293b6e053d898fc44fb405d4aae14b8
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2025-05-16 22:42:46 +0530

    Error and not found

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -2,6 +2,13 @@ import { getCabin } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
+export async function generateMetadata({ params: { cabinId } }) {
+  const cabin = await getCabin(cabinId);
+  return {
+    title: `Cabin ${cabin?.name}`,
+  };
+}
+
 export default async function Page({ params: { cabinId } }) {
   const cabin = await getCabin(cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
