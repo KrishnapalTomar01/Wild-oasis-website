c731f703adb24366c2c31056e6736f595df577c0
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2025-05-19 22:17:48 +0530

    Server to client

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -2,14 +2,30 @@ import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "../_lib/data-service";
 // import { unstable_noStore as noStore } from "next/cache";
 
-export default async function CabinList() {
+export default async function CabinList({ filter }) {
   // noStore(); // Opting out component from caching
   const cabins = await getCabins();
 
-  if (!cabins?.length) return null;
+  let displayCabins;
+  if (filter === "all") {
+    displayCabins = cabins;
+  }
+  if (filter === "small") {
+    displayCabins = cabins.filter((cabin) => cabin.maxCapacity <= 3);
+  }
+  if (filter === "medium") {
+    displayCabins = cabins.filter(
+      (cabin) => cabin.maxCapacity >= 4 && cabin.maxCapacity <= 7
+    );
+  }
+  if (filter === "large") {
+    displayCabins = cabins.filter((cabin) => cabin.maxCapacity >= 8);
+  }
+
+  if (!displayCabins?.length) return null;
   return (
     <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-      {cabins.map((cabin) => (
+      {displayCabins.map((cabin) => (
         <CabinCard cabin={cabin} key={cabin.id} />
       ))}
     </div>
