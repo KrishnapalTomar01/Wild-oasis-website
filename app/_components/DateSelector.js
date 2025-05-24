726f0743830279794a8498620bf7e9c73ee5d0bd
Krishnapal Tomar <krishnapal.01tomar@gmail.com>
2025-05-25 00:18:04 +0530

    Date range fix

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,12 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInCalendarDays,
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "./ReservationContext";
@@ -17,11 +23,12 @@ function isAlreadyBooked(range, datesArr) {
 
 function DateSelector({ settings, bookedDates, cabin }) {
   const { range, setRange, resetRange } = useReservation();
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const { regularPrice, discount } = cabin;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
+  const cabinPrice = numNights * (regularPrice - discount);
 
   // SETTINGS
   const { minBookingLength, maxBookingLength } = settings;
@@ -37,9 +44,13 @@ function DateSelector({ settings, bookedDates, cabin }) {
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
         onSelect={setRange}
-        selected={range}
+        selected={displayRange}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
@@ -70,7 +81,7 @@ function DateSelector({ settings, bookedDates, cabin }) {
           ) : null}
         </div>
 
-        {range.from || range.to ? (
+        {displayRange.from || displayRange.to ? (
           <button
             className="border border-primary-800 py-2 px-4 text-sm font-semibold"
             onClick={resetRange}
